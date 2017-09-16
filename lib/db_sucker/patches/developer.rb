3f3d769c7f719e47784303a4cf843d67e6b8ef2a
Sven Pachnit <sven@bmonkeys.net>
2017-09-16 16:08:04 +0200

    Dump core if more than one thread remains (via developer hooks)

--- lib/db_sucker/patches/developer.rb
+++ lib/db_sucker/patches/developer.rb
@@ -7,13 +7,14 @@ module DbSucker
       def self.hook!(app)
         app.hook :core_shutdown do
           app.debug "RSS: #{app.human_bytes(`ps h -p #{Process.pid} -o rss`.strip.split("\n").last.to_i * 1024)}"
+          app.sklaventreiber.window.keypad.dump_core if Thread.list.length > 1
         end
 
-        app.hook :worker_routine_before do |_, routine|
+        app.hook :worker_routine_before do |_, worker, routine|
           app.debug "RSS-before-#{routine}: #{app.human_bytes(`ps h -p #{Process.pid} -o rss`.strip.split("\n").last.to_i * 1024)}"
         end
 
-        app.hook :worker_routine_after do |_, routine|
+        app.hook :worker_routine_after do |_, worker, routine|
           app.debug "RSS-after-#{routine}: #{app.human_bytes(`ps h -p #{Process.pid} -o rss`.strip.split("\n").last.to_i * 1024)}"
         end
       end
