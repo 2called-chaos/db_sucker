fc28d21ae0f0d913d44903360f31e793ed370eeb
Sven Pachnit <sven@bmonkeys.net>
2017-09-18 00:22:43 +0200

    Add :cancel and :quit(!) commands

--- lib/db_sucker/application/window/keypad.rb
+++ lib/db_sucker/application/window/keypad.rb
@@ -56,10 +56,11 @@ module DbSucker
               @prompt.handle_input(ch)
             else
               case ch
-              when "^" then eval_prompt # (development)
               when ":" then main_prompt
-              when "T" then dump_core # (development)
+              when "^" then eval_prompt # (@development)
+              when "T" then dump_core # (@development)
               when "P" then kill_ssh_poll
+              when "?" then show_help
               when "q" then quit_dialog
               when "Q" then $core_runtime_exiting = 1
               end
@@ -67,19 +68,21 @@ module DbSucker
           end
         end
 
-        def eval_prompt
-          prompt!("eval> ") do |evil|
-            break if evil.blank?
-            app.dump_file "eval-result", true do |f|
-              begin
-                f.puts("#{evil}\n\n")
-                f.puts(app.sync{ app.instance_eval(evil) })
-              rescue StandardError => ex
-                f.puts("#{ex.class}: #{ex.message}")
-                ex.backtrace.each {|l| f.puts("  #{l}") }
-              end
+        def _eval evil
+          return if evil.blank?
+          app.dump_file "eval-result", true do |f|
+            begin
+              f.puts("#{evil}\n\n")
+              f.puts(app.sync{ app.instance_eval(evil) })
+            rescue StandardError => ex
+              f.puts("#{ex.class}: #{ex.message}")
+              ex.backtrace.each {|l| f.puts("  #{l}") }
             end
           end
+        end
+
+        def eval_prompt
+          prompt!("eval> ") {|evil| _eval(evil) }
         end
 
         def quit_dialog
@@ -94,18 +97,51 @@ module DbSucker
             end
           end
         end
+
+        def show_help
+
+        end
+
         def main_prompt
-          prompt!(":") do |evil|
-            break if evil.blank?
-            # app.dump_file "eval-result", true do |f|
-            #   begin
-            #     f.puts("#{evil}\n\n")
-            #     f.puts(app.sync{ app.instance_eval(evil) })
-            #   rescue StandardError => ex
-            #     f.puts("#{ex.class}: #{ex.message}")
-            #     ex.backtrace.each {|l| f.puts("  #{l}") }
-            #   end
-            # end
+          prompt!(":") do |raw|
+            break if raw.blank?
+            args = raw.split(" ")
+            cmd = args.shift
+            case cmd
+              when "c", "cancel" then cancel_workers(args)
+              when "q", "quit" then quit_dialog
+              when "q!", "quit!" then $core_runtime_exiting = 1
+              when "dump" then dump_core
+              when "eval" then args.any? ? _eval(args.join(" ")) : eval_prompt
+            end
+          end
+        end
+
+        def cancel_workers args
+          if args[0].is_a?(String)
+            sklaventreiber.sync do
+              if args[0] == "--all"
+                sklaventreiber.workers.each{|w| w.cancel! "canceled by user" }
+              else
+                # find worker
+                wrk = sklaventreiber.workers.detect do |w|
+                  if args[0].start_with?("^")
+                    w.table.match(/#{args[0]}/i)
+                  elsif args[0].start_with?("/")
+                    w.table.match(/#{args[0][1..-1]}/i)
+                  else
+                    w.table == args[0]
+                  end
+                end
+                if wrk
+                  wrk.cancel! "canceled by user"
+                else
+                  prompt!("Could not find any worker by the pattern `#{args[0]}'", color: :red)
+                end
+              end
+            end
+          else
+            prompt!("Usage: :c(cancel) <table_name|--all>", color: :yellow)
           end
         end
 
--- lib/db_sucker/application/window/prompt.rb
+++ lib/db_sucker/application/window/prompt.rb
@@ -2,7 +2,7 @@ module DbSucker
   class Application
     class Window
       class Prompt
-        attr_reader :cpos, :label, :buffer
+        attr_reader :cpos, :label, :buffer, :opts
 
         def initialize window, keypad
           @window = window
@@ -29,12 +29,21 @@ module DbSucker
           @label = nil
           @callback = nil
           @cpos = 0
+          @opts = {
+            color: :blue,
+            return_on_buffer: false,
+            return_on_enter: true,
+            has_cursor: true,
+            reset_on_escape: true,
+            prompt: nil, # proc
+          }
         end
 
-        def set! label, &callback
+        def set! label, opts = {}, &callback
           @label = label
           @callback = callback
           @active = true
+          @opts = @opts.merge(opts)
           @window.set_cursor(2)
           @keypad.app.fire(:prompt_start, @label)
         end
@@ -46,7 +55,11 @@ module DbSucker
             setpos(line, 0)
             clrtoeol
             setpos(line, 0)
-            blue(_this.label)
+            if _this.opts[:prompt]
+              instance_eval(&_this.opts[:prompt])
+            else
+              send(_this.opts[:color], _this.label)
+            end
             white(_this.buffer)
             force_cursor(line, stdscr.curx + _this.cpos)
           end
@@ -54,16 +67,20 @@ module DbSucker
 
         def handle_input ch
           case ch
-          when 27 then _escape
-          when 127 then _backspace
-          when 330 then _delete
-          when 260 then _left_arrow
-          when 261 then _right_arrow
-          when 13 then _enter
-          else
-            #@buffer.concat(ch.bytes.to_s)
-            @cpos.zero? ? @buffer.concat(ch) : @buffer.insert(@cpos - 1, ch)
+            when 27 then (@opts[:reset_on_escape] ? _escape : sbuf(ch))
+            when 127 then (@opts[:has_cursor] ? _backspace : sbuf(ch))
+            when 330 then (@opts[:has_cursor] ? _delete : sbuf(ch))
+            when 260 then (@opts[:has_cursor] ? _left_arrow : sbuf(ch))
+            when 261 then (@opts[:has_cursor] ? _right_arrow : sbuf(ch))
+            when 13 then (@opts[:return_on_enter] ? _enter : sbuf(ch))
+            else sbuf(ch)
           end
+        end
+
+        def sbuf ch
+          #@buffer.concat(ch.bytes.to_s)
+          @cpos.zero? ? @buffer.concat(ch) : @buffer.insert(@cpos - 1, ch)
+          _enter if @opts[:return_on_buffer]
         end
 
         def _escape
@@ -89,8 +106,10 @@ module DbSucker
         end
 
         def _enter
-          @callback.call(@buffer)
+          _b = @buffer
+          _c = @callback
           reset_state
+          _c.call(_b)
         end
       end
     end
